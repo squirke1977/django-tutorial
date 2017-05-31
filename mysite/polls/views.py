49fd583dc8594af139269af5e16d87cb761ec9d5
Steve Quirke (Stinkpad) <stephen.quirke1977@gmail.com>
2017-05-31 11:03:05 +0100

    End of tutorial 4 (and I'm skipping 5)

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,8 +5,25 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.views import generic
 
 from .models import Question, Choice
+
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
+	
+	def get_queryset(self):
+		"""Return the last five published questions"""
+		return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
 
 # Create your views here.
 def index(request):
